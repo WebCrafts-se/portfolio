9bd4ad605807ea6b142ba52f92d555a1f5e2e815
oliviaaMach <olivia.mach@outlook.com>
2026-01-27 10:39:19 +0100

    fixed navbar

--- js-files/hamburgerToggle.js
+++ js-files/hamburgerToggle.js
@@ -1,6 +1,7 @@
 export function hamburgerToggle(hamburgerSelector, navSelector) {
   const hamburger = document.querySelector(hamburgerSelector);
   const nav = document.querySelector(navSelector);
+  const navLinks = nav?.querySelectorAll("a");
 
   if (!hamburger || !nav) {
     console.warn("Hamburger menu: element not found.");
@@ -10,5 +11,12 @@ export function hamburgerToggle(hamburgerSelector, navSelector) {
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     nav.classList.toggle("open");
+  });
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      nav.classList.remove("open");
+    });
   });
 }
